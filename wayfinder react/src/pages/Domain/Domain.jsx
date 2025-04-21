9fe92f6f7b05105bc9b21334b989b7cea38a49e8
logan lugez <binglei.pro@gmail.com>
2025-04-21 12:20:08 +0200

    design domain page

--- wayfinder react/src/pages/Domain/Domain.jsx
+++ wayfinder react/src/pages/Domain/Domain.jsx
@@ -1,6 +1,7 @@
 import { Links, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { API_URL } from "../../../constants";
+import "./Domain.css"
 
 const DomainPage = () => {
   const { domainId } = useParams();
@@ -24,19 +25,35 @@ const DomainPage = () => {
   if (!domain) return <p>Chargement...</p>;
 
   return (
-    <div>
-      <h1>{domain.name}</h1>
-      <p>{domain.description}</p>
-      <ul>
-        {domain.fit_description.map((fit) => (
-          <li>{fit}</li>
-        ))}
-      </ul>
-      <ul>
-        {domain.real_tasks.map((task) => (
-          <li>{task}</li>
-        ))}
-      </ul>
+    <div className="domain-main">
+      <div className="domain-header">
+        <h1>{domain.name}</h1>
+        <button class="hero-button">Accéder au plan</button>
+      </div>
+      <div className="domain-body">
+        <div className="domain-infos">
+          <div className="domain-fit">
+            <h3>Ce métier est fait pour toi si :</h3>
+            <ul>
+              {domain.fit_description.map((fit) => (
+                <li>{fit}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="domain-tasks">
+            <h3>Ce que tu fera concrétement :</h3>
+            <ul>
+              {domain.real_tasks.map((task) => (
+                <li>{task}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div class="domain-description">
+          <h3>A propos</h3>
+          <p>{domain.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
